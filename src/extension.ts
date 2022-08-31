1def0116bba831c80c5a6e62eb21b89dab470eba
Julien Duchesne <julien.duchesne@grafana.com>
2022-08-31 09:13:56 -0400

    Dynamic Configuration Use the `DidChangeConfiguration` endpoint to dynamically configure the language server This allows the server to be configured without being restarted Fixes https://github.com/grafana/vscode-jsonnet/issues/14 Fixes https://github.com/grafana/vscode-jsonnet/issues/10

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as os from 'os';
 import { stringify as stringifyYaml } from 'yaml';
 
 import {
+	DidChangeConfigurationNotification,
 	Executable,
 	ExecuteCommandParams,
 	ExecuteCommandRequest,
@@ -23,10 +24,15 @@ export async function activate(context: ExtensionContext): Promise<void> {
 	extensionContext = context;
 
 	await startClient();
+	await didChangeConfigHandler();
 
 	context.subscriptions.push(
-		workspace.onDidChangeConfiguration(restartHandler),
-		commands.registerCommand('jsonnet.restartLanguageServer', restartHandler),
+		workspace.onDidChangeConfiguration(didChangeConfigHandler),
+		commands.registerCommand('jsonnet.restartLanguageServer', async function (): Promise<void> {
+			await client.stop();
+			client.outputChannel.dispose();
+			await startClient();
+		}),
 		commands.registerCommand('jsonnet.evalItem', async () => {
 			// Not enabled for now, because the language server doesn't support it.
 			const editor = window.activeTextEditor;
@@ -118,7 +124,7 @@ async function startClient(): Promise<void> {
 		command: binPath,
 		args: args,
 		options: {
-			env: Object.assign(process.env, { "JSONNET_PATH": jpath.join(path.delimiter) }),
+			env: process.env,
 		},
 	};
 	channel.appendLine(`Jsonnet Language Server will start: '${executable.command} ${executable.args.join(' ')}'`);
@@ -131,7 +137,7 @@ async function startClient(): Promise<void> {
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for jsonnet files
-		documentSelector: [{ scheme: 'file', language: 'jsonnet' }]
+		documentSelector: [{ scheme: 'file', language: 'jsonnet' }],
 	};
 
 	// Create the language client and start the client.
@@ -146,8 +152,19 @@ async function startClient(): Promise<void> {
 	client.start();
 }
 
-async function restartHandler(): Promise<void> {
-	await client.stop();
-	client.outputChannel.dispose();
-	await startClient();
+async function didChangeConfigHandler() {
+	const workspaceConfig = workspace.getConfiguration('jsonnet');
+	let jpath: string[] = workspaceConfig.get('languageServer.jpath');
+	jpath = jpath.map(p => path.isAbsolute(p) ? p : path.join(workspace.workspaceFolders[0].uri.fsPath, p));
+	client.sendNotification(DidChangeConfigurationNotification.type, {
+		settings: {
+			log_level: workspaceConfig.get('languageServer.logLevel'),
+			ext_vars: workspaceConfig.get("languageServer.extVars"),
+			jpath: jpath,
+			resolve_paths_with_tanka: workspaceConfig.get('languageServer.tankaMode'),
+			enable_lint_diagnostics: workspaceConfig.get('languageServer.lint'),
+			enable_eval_diagnostics: workspaceConfig.get('languageServer.eval'),
+			formatting: workspaceConfig.get('languageServer.formatting'),
+		}
+	});
 }
